648d04f7c5b7b4e82b46d81c572e2dea67da40a7
Gaats <rodrigocodignoli@gmail.com>
2026-09-24 15:36:13 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: https://kevcrzjzokwuhlvbwjsf.supabase.co,
-  SUPABASE_ANON_KEY: sb_publishable_Kwu_lL9_vnT0zv-fmSzZkQ_mLDNe7EI
+  SUPABASE_URL: "https://kevcrzjzokwuhlvbwjsf.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_Kwu_lL9_vnT0zv-fmSzZkQ_mLDNe7EI"
 };
